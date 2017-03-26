c51ad4fea9453384c96d1e224ece19297c8290b1
Travis.Riesenberg <travis.riesenberg@kiewit.com>
2017-03-25 20:47:16 -0500

    removing phones and lines from get_all_object_types_by_site, also added get ldap sync functionality

--- src/Callmanager.php
+++ src/Callmanager.php
@@ -155,32 +155,46 @@ class Callmanager
                     ];
     }
 
-    // Kick off LDAP Sync Process in CUCM - Name of LDAP Dirctory and true to start or false to stop.
-
-    public function do_ldap_sync($NAME, $BOOLEAN)
+    
+	// Kick off LDAP Sync Process in CUCM - Name of LDAP Dirctory and true to start or false to stop. 
+	
+	public function do_ldap_sync($NAME, $BOOLEAN)
     {
-        $SEARCH = ['name' => $NAME, 'sync' => $BOOLEAN];
+		$SEARCH = ['name' => $NAME, 'sync' => $BOOLEAN];
         // Search the CUCM for all phones
         $BASETIME = $this->microtimeTicks();
         $RETURN = $this->SOAPCLIENT->doLdapSync($SEARCH);
         $DIFFTIME = $this->microtimeTicks() - $BASETIME;
         // log our soap call
         $this->log_soap_call('doLdapSync', $DIFFTIME, $SEARCH, $RETURN);
+        
+		if (!is_object($RETURN)) {
+            throw new \Exception('SOAP reply is not an object');
+        }else{
+			return $RETURN;
+		}
+    }
+	
+	public function get_ldap_sync_status()
+    {
+
+        // Search the CUCM for all phones
+        $BASETIME = $this->microtimeTicks();
+        $RETURN = $this->SOAPCLIENT->getLdapSync();
+        $DIFFTIME = $this->microtimeTicks() - $BASETIME;
+        // log our soap call
+        $this->log_soap_call('getLdapSync', $DIFFTIME, $RETURN);
 
         if (!is_object($RETURN)) {
             throw new \Exception('SOAP reply is not an object');
         } else {
             return $RETURN;
         }
-        // Decode the reply into an array of results
-        //$RETURN = $this->decode_soap_reply($RETURN);
-        // Turn the associative arrays into a single simensional array list
-        //$RETURN = $this->assoc_key_values_to_array($RETURN, 'name');
     }
 
-    // Get a complete list of the names of all phones
-
-    public function get_phone_names()
+	// Get a complete list of the names of all phones
+    
+	public function get_phone_names()
     {
         $SEARCH = $this->axl_search_return_array(['devicePoolName' => '%'], ['name' => '']);
         // Search the CUCM for all phones
@@ -520,8 +534,17 @@ class Callmanager
         // Get our valid object types
         $TYPES = $this->object_types();
 
+        // Do not get these objects by site. - They need to be requested directly. 
+		$DISCARD_TYPES = [
+					'Phone',
+					'Line',
+					];
+			
         $RETURN = [];
         foreach ($TYPES as $TYPE) {
+			if (in_array($TYPE, $DISCARD_TYPES)){
+				continue;
+			}
             if (php_sapi_name() === 'cli') {
                 //print "Getting {$SITE}s {$TYPE}...".PHP_EOL;
             }
